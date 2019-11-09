28550922313618842407dda850d93d0c64bc8cb2
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-11-09 14:08:57 -0800

    added carousel to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,70 @@
-import React, { Fragment } from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+import Slider from "react-slick";
 import "./Home.css";
 
-const Home = () => (
-  <Fragment>
-    <Title>BeyondDjs</Title>
-    <button class="btn"><Link to="/login">ENTER</Link></button>
-  </Fragment>
-)
+// const Home = () => (
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  }
+
+class Home extends React.Component {
+
+  
+  render() {
+    
+    
+    return (
+      <div class="autoplay slider">
+      <Title>BeyondDjs</Title>
+      <Slider {...settings}>
+        <div>
+          <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+        <div>
+        <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+        <div>
+        <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+        <div>
+        <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+        <div>
+        <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+        <div>
+        <img
+              src="http://placehold.it/960x300?text=1-960w 960w"   
+          />
+        </div>
+      </Slider>
+      <button class="btn"><Link to="/login">ENTER</Link></button>
+      </div>
+    );
+  }
+    
+  }
+//)
 
 export default Home;
