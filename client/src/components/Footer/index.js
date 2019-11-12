2458ec7cd0cacfff7ac47dedecf93b50ec5ee949
Kathleen Leung <kathleenleungky@gmail.com>
2019-11-12 15:47:15 -0800

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container-fluid" id="end">
+                <Row>
+                    <Col sm md lg={6} id="media">
+                        <a><i className="fa fa-github" style={{ fontSize: "48px" }}></i></a>
+                        <a><i className="fa fa-facebook-square" style={{ fontSize: "48px" }}></i></a>
+                        <a><i className="fa fa-twitter" style={{ fontSize: "48px" }}></i></a>
+                        <a><i className="fa fa-instagram" style={{ fontSize: "48px" }}></i></a>
+                    </Col>
+                    <Col sm md lg={3} id="links" className="mt-3">
+                        <Link to="/about" className="float-right">ABOUT US</Link>
+                        <br />
+                        <Link to="/contact" className="float-right">CONTACT</Link>
+                        <br />
+                        <Link to="/contact" className="float-right">TEAM</Link>
+                    </Col>
+                    <Col sm md lg={3} className="mt-3">
+                        <p id="BeyondDJ" className="m-0">BeyondDJ</p>
+                        <p>&copy; 2019. All rights reserved.</p>
+                    </Col>
+                </Row>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Title from "../components/Title";
 import Slider from "../components/Slider";
 import UserBox from "../components/UserBox";
+import Footer from "../components/Footer";
 import { Container, Row, Col, Nav, Navbar, Button, ButtonToolbar } from 'react-bootstrap';
 // Import css files
 // import "slick-carousel/slick/slick.css";
@@ -14,34 +15,41 @@ class Home extends React.Component {
   render() {
 
     return (
-      <Container fluid className="p-0">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="/" id="BeyondDJ"><span><img src="./imgs/disco.png" alt="disco ball" id="disco" /></span>&nbsp;BeyondDJ</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <Nav>
-              <ButtonToolbar>
-                <Button href="/login" variant="outline-light">Log In</Button>
-                <Button href="/register" variant="outline-light">Sign Up</Button>
-              </ButtonToolbar>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Slider></Slider>
-        <Row className="mt-5">
-          <Col className="p-0">
-            <UserBox src="./imgs/promoter.png" description="Concert Promoters" btn="Book Talent">
-              Are you a Promoter looking to book a DJ? Join us to discover a natural born talent that fits best for you!
+      <>
+        <Container fluid className="p-0">
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/" id="BeyondDJ"><span><img src="./imgs/disco.png" alt="disco ball" id="disco" /></span>&nbsp;BeyondDJ</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+              <Nav>
+                <ButtonToolbar>
+                  <Button href="/login" variant="outline-light">Log In</Button>
+                  <Button href="/register" variant="outline-light">Sign Up</Button>
+                </ButtonToolbar>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Slider></Slider>
+          <Row className="mt-5">
+            <Col className="p-0">
+              <UserBox src="./imgs/promoter.png" description="Concert Promoters" btn="Book Talent">
+                Are you a Promoter looking to book a DJ? Join us to discover a natural born talent that fits best for you!
             </UserBox>
-          </Col>
-          <Col className="p-0">
-            <UserBox src="./imgs/dj.png" description="Artists" btn="Find Gigs">Are you a DJ looking to join our platform? Find local and national gigs and touring opportunities</UserBox>
-          </Col>
-          <Col className="p-0">
-            <UserBox src="./imgs/basic.png" description="Basic Users" btn="Register">Not an entertainer or a promoter but a music fanatic that wants to check what's in it for you?</UserBox>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+            <Col className="p-0">
+              <UserBox src="./imgs/dj.png" description="Artists" btn="Find Gigs">Are you a DJ looking to join our platform? Find local and national gigs and touring opportunities</UserBox>
+            </Col>
+            <Col className="p-0">
+              <UserBox src="./imgs/basic.png" description="Basic Users" btn="Register">Not an entertainer or a promoter but a music fanatic that wants to check what's in it for you?</UserBox>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            </Col>
+          </Row>
+        </Container>
+        <Footer></Footer>
+      </>
     );
 
   }
