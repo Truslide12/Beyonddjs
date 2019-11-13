4d234ad25ee8ddaf5af3c57be9a97cce55fb55ba
Daniel Spira <daniel.b.spira@gmail.com>
2019-11-13 12:03:20 -0800

    updated calendar component to allow users to specify which kind of event they're looking to book a DJ for. also added a submit button that grabs the values from the form and stores them in variables so we can use that to make calls from the database.

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -6,8 +6,24 @@ class TestCalendar extends Component {
     this.state = {
       date: '',
       time: '',
-      duration: '1'
+      duration: '1',
+      eventType: 'Wedding'
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    let date = this.state.date;
+    let time = this.state.time;
+    let duration = this.state.duration;
+    let eventType = this.state.eventType;
+    alert('A ' + eventType + ' DJ request was submitted for a ' + duration + ' hour set on ' + date + ' at '+ time + '.');
+    event.preventDefault();
   }
 
   dateChange = event => {
@@ -23,24 +39,39 @@ class TestCalendar extends Component {
   }
 
   durationChange = event => {
-    console.info(event)
     this.setState({
       duration: event.target.value
+    });
+  }
+
+  eventTypeChange = event => {
+    this.setState({
+      eventType: event.target.value
     });
   }
 
   render() {
     return (
       <div>
-        <input type='date' value={this.state.date} onChange={this.dateChange} />
-        <input type='time' value={this.state.time} onChange={this.timeChange} />
-        <select value={this.state.duration} onChange={this.durationChange}>
-          <option value="1">1 hrs</option>
-          <option value="2">2 hrs</option>
-          <option value="3">3 hrs</option>
-          <option value="4">4 hrs</option>
-          <option value="5">5 hrs</option>
-        </select>
+        <form ref="form" onSubmit={this.handleSubmit}>
+          <input type='date' value={this.state.date} onChange={this.dateChange} />
+          <input type='time' value={this.state.time} onChange={this.timeChange} />
+          <select value={this.state.duration} onChange={this.durationChange}>
+            <option value="1">1 hrs</option>
+            <option value="2">2 hrs</option>
+            <option value="3">3 hrs</option>
+            <option value="4">4 hrs</option>
+            <option value="5">5 hrs</option>
+          </select>
+          <select value={this.state.eventType} onChange={this.eventTypeChange}>
+            <option value="Wedding">Wedding</option>
+            <option value="Bar Mitzvah">Bar Mitzvah</option>
+            <option value="Karaoke">Karaoke</option>
+            <option value="Club/Bar">Club/Bar</option>
+            <option value="Other Event">Other Event</option>
+          </select>
+          <button type="submit">Search for DJs</button>
+        </form>
       </div>
     );
   }
