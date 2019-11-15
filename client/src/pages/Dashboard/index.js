1961de29a8406337c27ca5d7e40305495814903c
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-11-14 19:32:33 -0800

    setup mongo and inputs from the database. changed from individual pages to use a single dashboard. Need to add in api calls to replace the direct calls in the dashboard index.js file

---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,106 @@
+import React, { Component, Fragment } from "react";
+import cookie from 'react-cookies';
+import { Redirect } from 'react-router-dom';
+import Title from "../../components/Title/index";
+import API from "../../utils/API";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cookie: null,
+      loading: true,
+      email: '',
+      password: '',
+      role: '',
+      firstName: '',
+      lastName: '',
+      city: '',
+      state: '',
+      zip: '',
+      phone: '',
+      calendar: [],
+    };
+  }
+
+  componentDidMount() {
+    this.validateCookie();
+    // this.generateForm();
+    console.log(this.session);
+  }
+
+  validateCookie() {
+    const cookieValue = cookie.load('connect.sid');
+    API.validateCookie(cookieValue)
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({ 
+            cookie: cookieValue, loading: false,
+            // email: user.email,
+            // role: user.role,
+            // firstName: user.firstName,
+            // lastName: user.lastName,
+            // phone: user.phone,
+            // calendar: user.calendar,
+          });
+        } else {
+          this.setState({ loading: false });
+        }
+      })
+      .catch(err => this.setState({ loading: false }))
+  }
+
+  handleLogout () {
+    API.logout()
+      .then(res => this.props.history.push('/login'))
+      .catch(err => console.error(err));
+  }
+
+  render(props) {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
+    if (!this.state.cookie) {
+      return <Redirect to='/login' />
+    }
+    if (this.state.role === 'Admin') {
+    return (
+      <Fragment>
+        <Title>This the Admin page</Title>
+        <p>Hello: {props.email}</p>
+        <button id="logout" onClick={this.handleLogout} className="btn">LOG OUT</button>
+      </Fragment>
+    );
+    }
+    else if (this.state.role === 'Entertainer') {
+    return (
+      <Fragment>
+        <Title>This the Entertainer page</Title>
+        <p>Hello: {props.email}</p>
+        <button id="logout" onClick={this.handleLogout} className="btn">LOG OUT</button>
+      </Fragment>
+    );
+    }
+    else if (this.state.role === 'promoterVendor') {
+    return (
+      <Fragment>
+        <Title>This the Promoter/Vendor page</Title>
+        <p>Hello: {props.email}</p>
+        <button id="logout" onClick={this.handleLogout} className="btn">LOG OUT</button>
+      </Fragment>
+    );
+    }
+    else
+    if (this.state.role) {
+      return (
+        <Fragment>
+          <Title>This the User page</Title>
+          <p>Hello: {props.email}</p>
+          <button id="logout" onClick={this.handleLogout} className="btn">LOG OUT</button>
+        </Fragment>
+      );
+      }
+  }
+}
+
+export default Dashboard;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,29 @@
 export default {
-  login: (username, password) => 
+  login: (email, password) => 
     fetch("/api/auth/login", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password })
+      body: JSON.stringify({ email, password })
     }),
-    register: (username, password) =>
+    register: ( email, password, role, firstName, lastName, city, state, zip, phone) =>
     fetch("/api/auth/register", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password })
+      body: JSON.stringify({ email, password, role, firstName, lastName, city, state, zip, phone })
     }),
   logout: () => fetch("/api/auth/", { method: 'DELETE' }),
   validateCookie: sid => fetch(`/api/auth/session/${encodeURIComponent(sid)}`),
+  // event: () =>    //add appropriate event info from form
+  //   fetch("api/auth/event", {
+  //     method: postMessage,
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({}) // add appropriate event info calls here too
+  //   }),
+  // search: (zip, date, startTime, EndTime) => { // add all search info to run search
+  // fetch("api/auth/search"), {
+  //   method: 'Post',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ zip, date, startTime, EndTime })
+  // }
+  // }
 };
