ff4afa5fa85c8cde2ac3ba837560d8297352b5e4
Summer <316summer@gmail.com>
2019-11-15 19:57:32 -0800

    added validation to Log in page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -6,14 +6,18 @@ import Title from "../../components/Title";
 import Form from "../../components/LoginForm";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import "./Login.css";
+import { FormErrors } from "./FormErrors";
 
 class Login extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      formErrors: {email: '', password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -22,9 +26,39 @@ class Login extends Component {
 
   handleInputChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value },
+    () => { this.validateField(name, value) });
+  }
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
   }
 
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
+  }
   handleSubmit(event) {
     event.preventDefault();
 
@@ -43,6 +77,7 @@ class Login extends Component {
             <Row>
               <Col>
                 <div id="registerBox">
+                <FormErrors formErrors={this.state.formErrors} />
                   <h1 id="signUp">Sign Up Right Now!</h1>
                   <p id="regIntro">First Timer? Create an account with us to get access to one of the live music marketplace that makes it easy for talent buyers, event planners, venues and individuals to find, book, and pay musicians and bands. We dream of a world with more live music than ever because we’ve made it easy to find and book the right talent! What are you waiting for? Join us today!</p>
                   <Button href="/register" variant="light" className="m-0">REGISTER</Button>
