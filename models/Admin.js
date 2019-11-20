6ef4fe8270e9f8cf7bc91a9a3d45408b4f9ed77b
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-11-20 15:01:52 -0800

    create initial models for Admin Entertainer and PromoterVentor collections

---
+++ models/Admin.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+    username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  viewAll: {
+    type: Boolean,
+    required: true
+  },
+  canEdit: {
+    type: Boolean,
+    required: true
+  },
+})
+
+const Admin = mongoose.model('Admin', AdminSchema);
+
+module.exports = Admin;
