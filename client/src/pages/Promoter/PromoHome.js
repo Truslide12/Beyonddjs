e5957ccf95a8ec45145f72b685ba188e420d3367
Kathleen Leung <kathleenleungky@gmail.com>
2019-11-19 20:15:37 -0800

    adding content on PromoHome page

--- client/src/pages/Promoter/PromoHome.js
+++ client/src/pages/Promoter/PromoHome.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Row, Col } from 'react-bootstrap';
+import { cpus } from "os";
 
 function PromoHome(){
     return (
         <div className="promoContent">
             <h1 id="welcome"><span className="BeyondDJ">Welcome to BeyondDJ!</span></h1>
-            
+            <Row>
+                <Col>
+                </Col>
+            </Row>
         </div>
     );
 }
