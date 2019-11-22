bb85072c9d31d217ae4b57560a62c9460f7abbbf
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-11-21 17:41:45 -0800

    created entertainer and admin folders with files

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import cookie from 'react-cookies';
 import { Redirect } from 'react-router-dom';
 import Title from "../../components/Title/index";
-import Availability from "../../components/Availability";
+
 import TestCalendar from "../../components/Calendar";
 import PromoterPage from "../Promoter";
 import API from "../../utils/API";
@@ -104,7 +104,7 @@ class Dashboard extends Component {
           <p>Email: {this.state.email}</p>
           <button id="logout" onClick={this.handleLogout} className="btn">LOG OUT</button> */}
           <PromoterPage firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} handleLogout={this.handleLogout} firstInitial={this.state.firstInitial} lastInitial={this.state.lastInitial}/>
-          <Availability />
+
         </Fragment>
       );
       }
---
+++ client/src/pages/Entertainer/EntertainerHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PromoHome(){
+    return (
+        <div className="promoContent">
+            <h1 id="welcome"><span className="BeyondDJ">Welcome to BeyondDJ!</span></h1>
+            
+        </div>
+    );
+}
+
+export default PromoHome;
