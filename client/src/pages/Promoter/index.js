23c98ba2aeb3429306388ac1c239e454c2d6b71b
Kathleen Leung <kathleenleungky@gmail.com>
2019-11-21 20:18:50 -0800

    fixed weird col of Availability

--- client/src/pages/Promoter/index.js
+++ client/src/pages/Promoter/index.js
@@ -122,9 +122,6 @@ class Basic extends Component {
                   </nav>
                 </div>
               </Col>
-              <Col>
-              <Availability />
-              </Col>
             </Row>
             <Row>
               <Col>
